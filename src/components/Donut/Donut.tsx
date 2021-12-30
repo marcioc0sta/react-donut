ed617a5a085050d7a1ff905c8cfe26ef9ed4beb1
marcioc0sta <marciocst2@gmail.com>
2021-12-30 19:56:34 -0300

    feat: handles active section

--- src/components/Donut/Donut.tsx
+++ src/components/Donut/Donut.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, memo, useRef, useState } from 'react'
+import React, { useMemo, memo, useState } from 'react'
 import { useDonutCoordinates } from '../../hooks'
 import {
   D3parse,
@@ -22,6 +22,22 @@ const sectionOpacityAmount = {
   INACTIVE: '0.6',
 }
 
+const parseData = (data: DataToParse, donutRadius: number): D3parse => {
+  const valuesArr = data.map((item: DonutSectionObject) => item.value)
+  const total = valuesArr.reduce((accum, current) => accum + current)
+
+  const getDonutVal = (val: number) => (val * donutRadius) / total
+
+  const parsedData = data.map(item => ({
+    slug: item.slug,
+    title: item.label,
+    value: getDonutVal(item.value),
+    rawValue: item.value,
+  }))
+
+  return { parsedData, total }
+}
+
 const Donut: React.FC<DonutProps> = ({
   sectionColors,
   donutData,
@@ -30,24 +46,12 @@ const Donut: React.FC<DonutProps> = ({
 }) => {
   const donutRadius = 140
   const donutInnerRadius = 15
+  const [activeSection, setActiveSection] = useState('')
 
-  const parseData = (data: DataToParse): D3parse => {
-    const valuesArr = data.map((item: DonutSectionObject) => item.value)
-    const total = valuesArr.reduce((accum, current) => accum + current)
-
-    const getDonutVal = (val: number) => (val * donutRadius) / total
-
-    const parsedData = data.map(item => ({
-      slug: item.slug,
-      title: item.label,
-      value: getDonutVal(item.value),
-      rawValue: item.value,
-    }))
-
-    return { parsedData, total }
-  }
-
-  const { parsedData, total } = useMemo(() => parseData(donutData), [donutData])
+  const { parsedData, total } = useMemo(
+    () => parseData(donutData, donutRadius),
+    [donutData]
+  )
 
   const { arc, activeArc, sectionAngles } = useDonutCoordinates({
     invertData: false,
@@ -59,16 +63,37 @@ const Donut: React.FC<DonutProps> = ({
   const sectionAngleColorIndex = (currentSection: SectionType) =>
     donutData.findIndex(item => currentSection.data.slug === item.slug)
 
+  const handleActiveSection = (slug: string) => () => {
+    setActiveSection(slug)
+  }
+
+  const handleOpacity = (slug: string) => {
+    return slug !== activeSection && activeSection.length !== 0
+      ? sectionOpacityAmount.INACTIVE
+      : sectionOpacityAmount.ACTIVE
+  }
+
+  const handleArcs = (active: boolean, section: SectionType) => {
+    return active ? activeArc(section) : arc(section)
+  }
+
   return (
     <div style={{ position: 'relative' }}>
       <svg width='180' height='180'>
         <g style={{ transform: 'translate(90px, 90px)' }}>
           {sectionAngles.map((section: any) => {
+            const slug = section?.data?.slug ?? ''
+            const active = activeSection === slug
             return (
               <path
-                key={section.data.slug}
+                onMouseLeave={() => {
+                  setActiveSection('')
+                }}
+                onMouseEnter={handleActiveSection(slug)}
+                key={slug}
+                fillOpacity={handleOpacity(slug)}
                 fill={sectionColors[sectionAngleColorIndex(section)]}
-                d={arc(section) || ''}
+                d={handleArcs(active, section) || ''}
               />
             )
           })}
